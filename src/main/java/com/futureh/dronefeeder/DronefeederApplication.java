5037efc0491067fa7d3ef4417b928aaa842ba35d
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2022-06-23 21:08:33 -0300

    feat: teste docker

--- src/main/java/com/futureh/dronefeeder/DronefeederApplication.java
+++ src/main/java/com/futureh/dronefeeder/DronefeederApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DronefeederApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DronefeederApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DronefeederApplication.class, args);
+  }
 
 }
--- src/main/java/com/futureh/dronefeeder/infrastructure/persistence/hibernate/repository/drone/DroneRepository.java
+++ src/main/java/com/futureh/dronefeeder/infrastructure/persistence/hibernate/repository/drone/DroneRepository.java
@@ -1,8 +1,8 @@
 package com.futureh.dronefeeder.infrastructure.persistence.hibernate.repository.drone;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.futureh.dronefeeder.domain.drone.model.Drone;
+
+import org.springframework.data.repository.CrudRepository;
 
 public interface DroneRepository extends CrudRepository<Drone, Integer> {
 
