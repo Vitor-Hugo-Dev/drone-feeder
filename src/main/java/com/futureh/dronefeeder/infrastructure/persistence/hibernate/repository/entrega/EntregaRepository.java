6ef01a23e4db820c692b299b2b69c7246a9b2193
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2022-07-02 08:43:27 -0300

    fix: mudança do tipo de pk

--- src/main/java/com/futureh/dronefeeder/infrastructure/persistence/hibernate/repository/entrega/EntregaRepository.java
+++ src/main/java/com/futureh/dronefeeder/infrastructure/persistence/hibernate/repository/entrega/EntregaRepository.java
@@ -1,9 +1,9 @@
 package com.futureh.dronefeeder.infrastructure.persistence.hibernate.repository.entrega;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.futureh.dronefeeder.domain.entrega.model.Entrega;
 
-public interface EntregaRepository extends CrudRepository<Entrega, Long> {
+import org.springframework.data.repository.CrudRepository;
+
+public interface EntregaRepository extends CrudRepository<Entrega, Integer> {
 
 }
