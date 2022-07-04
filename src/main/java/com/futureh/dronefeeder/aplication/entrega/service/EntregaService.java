68a4b4a301d8b5ccf7ddaa347101a710ddae7bd8
Vitor Hugo <vitorhugo.dev1997@gmail.com>
2022-07-04 18:33:14 -0300

    feat: criando rota de post entrega e testes

--- src/main/java/com/futureh/dronefeeder/aplication/entrega/service/EntregaService.java
+++ src/main/java/com/futureh/dronefeeder/aplication/entrega/service/EntregaService.java
@@ -1,5 +1,52 @@
 package com.futureh.dronefeeder.aplication.entrega.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.futureh.dronefeeder.domain.entrega.exception.EntregaInvalidaException;
+import com.futureh.dronefeeder.domain.entrega.model.Entrega;
+import com.futureh.dronefeeder.infrastructure.persistence.hibernate.repository.entrega.EntregaRepository;
+
+@Service
 public class EntregaService {
-  
+
+  @Autowired
+  EntregaRepository entregaRepository;
+
+  public Entrega salvarEntrega(Entrega entrega)
+      throws EntregaInvalidaException {
+    verificaDadosDeEntrega(entrega);
+    Entrega newEntrega = entregaRepository.save(entrega);
+
+    return newEntrega;
+  }
+
+  private void verificaDadosDeEntrega(Entrega entrega)
+      throws EntregaInvalidaException {
+    if (entrega.getDrone() == null) {
+      throw new EntregaInvalidaException(
+          "drone não pode ser Null");
+    } else if (entrega.getStatusPedido() == null) {
+      throw new EntregaInvalidaException(
+          "statusPedido não pode ser Null");
+    } else if (entrega.getNomeCliente() == null) {
+      throw new EntregaInvalidaException(
+          "nomeCliente não pode ser Null");
+    } else if (entrega.getEnderecoEntrega() == null) {
+      throw new EntregaInvalidaException(
+          "enderecoEntrega não pode ser Null");
+    } else if (entrega.getDataChegadaPedido() == null) {
+      throw new EntregaInvalidaException(
+          "dataChegada não pode ser Null");
+    } else if (entrega.getDataEntregaPedido() == null) {
+      throw new EntregaInvalidaException(
+          "dataEntregaPedido não pode ser Null");
+    } else if (entrega.getHorarioRecebimentoPedido() == null) {
+      throw new EntregaInvalidaException(
+          "horarioRecebimentoPedido não pode ser Null");
+    } else if (entrega.getHorarioEntregaPedido() == null) {
+      throw new EntregaInvalidaException(
+          "horarioEntregaPedido não pode ser Null");
+    }
+  }
 }
---
+++ src/main/java/com/futureh/dronefeeder/domain/entrega/exception/EntregaInvalidaException.java
@@ -0,0 +1,8 @@
+package com.futureh.dronefeeder.domain.entrega.exception;
+
+public class EntregaInvalidaException extends Exception {
+
+  public EntregaInvalidaException(String message) {
+    super(message);
+  }
+}
